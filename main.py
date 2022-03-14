a0d657edf890f0778a5c160b2ecb1019cee6189b
nuclear06 <91713418+nuclear06@users.noreply.github.com>
2022-03-14 16:39:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('hello world')
+import logging
+
+logging.basicConfig(filename='Running_log', level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s - [line:%(lineno)d] ')
+logger = logging.getLogger('test')
+logger.error('test')
+logger.info('test')
+logger.warning('test')
+logger.debug('test')
