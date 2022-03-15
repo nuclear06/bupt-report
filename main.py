67e88e31b71a149caefab9f9aa8c7748d2d99a90
nuclear06 <91713418+nuclear06@users.noreply.github.com>
2022-03-15 15:53:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import logging
 from qq_email import error_mail
 from qq_email import right_mail
 
-right_mail(USERS[0],)
+temp1 = eval(USERS)[0]
+
 logging.basicConfig(filename='Running_log', level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s - [line:%(lineno)d] ')
 logger = logging.getLogger('test')
@@ -13,5 +14,5 @@ logger.debug('test')
 print('日志记录完毕')
 with open('Running_log') as f:
   temp = f.read()
-  right_mail(USER,temp)
+  right_mail(temp1,temp)
   
