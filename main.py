1bad48bfbd30f958076dedc314291abc51c99c61
nuclear06 <91713418+nuclear06@users.noreply.github.com>
2022-03-15 20:41:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import requests
-from data import Data
+from data import *
 from checks import *
-import json
 import time
 from parameter import *
 from qq_email import error_mail
@@ -13,11 +12,13 @@ def load_user():
     :return:返回用户数据
     """
     user_list = os.environ['USERS']
-    return user_list
+    post_data = os.environ['DATA']
+    return user_list, post_data
+    # with open('user.json') as f:
+    #     return json.load(f)
 
 
-
-def main(user):
+def main(user, post_data):
     flag = 0
     session = requests.Session()
     account = user['user']
@@ -27,53 +28,57 @@ def main(user):
         try:
 
             flag += 1
-            resp1 = session.post(check_url, headers=head, data=Data.get_logindata(account, pswd))
+            if flag > MAX_NUM and LOGGING:
+                error = '[{}]失败次数过多，其填报已终止'.format(user['id'])
+                raise ZeroDivisionError(error)
+            
+            resp1 = session.post(check_url, headers=head, data=get_logindata(account, pswd))
             # 登陆
             message = re_message.search(resp1.text).groups()[0]
             message_check(message)
             # 检测登陆信息
 
-
-            get_data = Data.get_history(session)
-            # 获得历史填报数据
-            # print(get_data)
-            # return
-            #######################################
+            get_data = get_postdata(post_data)
             resp2 = session.post(post_url, headers=head, data=get_data)
             # 填报数据
 
             if resp1.status_code != 200 and LOGGING:
                 main_logger.warning('用户[{}]登陆失败，状态码不是200'.format(id))
+                time.sleep(15)
+                # 延迟一段时间后重试
                 continue
             if resp2.status_code != 200 and LOGGING:
                 main_logger.warning('用户[{}]填报失败，状态码不是200'.format(id))
+                time.sleep(15)
+                # 延迟一段时间后重试
                 continue
-            if flag > MAX_NUM and LOGGING:
-                main_logger.error('填报失败次数过多，已终止')
-                error_mail(user, '填报失败次数过多，已终止')
-                raise Exception('失败次数过多')
+            
+            break
+        #     正常执行一次结束
 
-        except json.decoder.JSONDecodeError:
-            if LOGGING:
-                main_logger.error('用户[{}]历史数据获取失败'.format(id))
+        except ZeroDivisionError as mes:
+            print(mes)
+            main_logger.error(str(mes))
+            error_mail(user, str(mes))
+
         except RuntimeWarning as mes:
             print(mes)
+            error_mail(user, str(mes)+'请检查输入的账户')
             exit(-1)
         #     登陆时会出现的错误
         #     随便找了两个错误来捕捉
-        time.sleep(15)
-
     session.close()
 
 
 if __name__ == '__main__':
-    for i in load_user():
+    temple = load_user()
+    length = len(temple[0])
+    for i in range(length):
         try:
-            main(i
-            right_mail(i)
+            main(temple[0][i], temple[1][i])
+            right_mail(temple[0][i])
 
 
         except Exception as e:
             print(e)
-            other_logger.error('未知错误{}'.format(e.message))
-
+            other_logger.error(repr(e))
