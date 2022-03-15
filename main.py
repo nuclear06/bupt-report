c4f77b27d31cd256f5fa45e0b51c14bffeff9436
nuclear06 <91713418+nuclear06@users.noreply.github.com>
2022-03-15 17:34:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,86 @@
-import logging
+import requests
+from data import Data
+from checks import *
+import json
+import time
+from parameter import *
 from qq_email import error_mail
 from qq_email import right_mail
 
-import os
 
-users_list = eval(os.environ['USERS'])
+def load_user():
+    """
+    :return:返回用户数据
+    """
+    user_list = os.environ['USERS']
+    return user_list
 
-logging.basicConfig(filename='Running_log.log', level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s - [line:%(lineno)d] ')
-logger = logging.getLogger('test')
-logger.error('test')
-logger.info('test')
-logger.warning('test')
-logger.debug('test')
-print('日志记录完毕')
-with open('./Running_log.log') as f:
-  temp = f.read()
-  print(temp)
-  right_mail(users_list[0],temp)
-  
+
+
+def main(user):
+    flag = 0
+    session = requests.Session()
+    account = user['user']
+    pswd = user['pswd']
+    id = user['id']
+    while True:
+        try:
+
+            flag += 1
+            resp1 = session.post(check_url, headers=head, data=Data.get_logindata(account, pswd))
+            # 登陆
+            message = re_message.search(resp1.text).groups()[0]
+            message_check(message)
+            # 检测登陆信息
+
+
+            get_data = Data.get_history(session)
+            # 获得历史填报数据
+            # print(get_data)
+            # return
+            ####################################### resp2 = session.post(post_url, headers=head, data=get_data)
+            # 填报数据
+
+            if resp1.status_code != 200 and LOGGING:
+                main_logger.warning('用户[{}]登陆失败，状态码不是200'.format(id))
+            # if resp2.status_code != 200 and LOGGING:
+            #     main_logger.warning('用户[{}]填报失败，状态码不是200'.format(id))
+            if flag >= MAX_NUM and LOGGING:
+                main_logger.error('填报失败次数过多，已终止')
+                error_mail(user, '填报失败次数过多，已终止')
+                raise Exception('失败次数过多')
+
+        except json.decoder.JSONDecodeError:
+            if LOGGING:
+                main_logger.error('用户[{}]历史数据获取失败'.format(id))
+        except RuntimeWarning as mes:
+            print(mes)
+            exit(-1)
+        #     登陆时会出现的错误
+        #     随便找了两个错误来捕捉
+        time.sleep(15)
+
+    session.close()
+
+
+if __name__ == '__main__':
+    for i in load_user():
+        try:
+            main(i)
+            right_mail(i)
+
+
+        except Exception as e:
+            print(e)
+            other_logger.error('未知错误')
+    # if True:
+    # print('hello')
+
+    try:
+        if LOGGING:
+            log_check()
+    #         检测日志大小（预设最大10kb）
+    except NameError:
+        # 未检测到日志文件
+        other_logger.warning('未检测到日志文件')
+        pass
