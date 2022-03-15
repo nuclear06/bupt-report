ce524c4f0f0e3feaaa2e14ef487c96460a373164
nuclear06 <91713418+nuclear06@users.noreply.github.com>
2022-03-15 14:03:30 +0800

    Create qq_email.py

---
+++ qq_email.py
@@ -0,0 +1,51 @@
+from parameter import email_logger
+import smtplib
+from email.mime.text import MIMEText
+
+s = smtplib.SMTP_SSL('smtp.qq.com', 465)  # 邮件服务器及端口号
+
+
+def get_mail(msg_from,msg_to,flag,content='-UNKNOWN-'):
+
+    if flag:
+        subject = "晨午晚检自动打卡成功"  # 主题
+        cont = "打卡成功,日志信息：\n".format(content)  # 正文
+    else:
+        subject = "ERROR:晨午晚检打卡出现错误"  # 主题
+        cont = "错误信息：\n{}".format(content)  # 正文
+    msg = MIMEText(cont)
+    msg['Subject'] = subject
+    msg['From'] = msg_from
+    msg['To'] = msg_to
+    return msg
+
+
+def right_mail(user):
+    try:
+        msg_from = user["mail_from"]  # 发送方邮箱
+        passwd = user["mail_key"]  # 填入发送方邮箱的授权码
+        msg_to = user["mail_to"]  # 收件人邮箱
+        msg = get_mail(msg_from,msg_to,True)
+        s.login(msg_from, passwd)
+        s.sendmail(msg_from, msg_to, msg.as_string())
+        print("发送成功")
+    except smtplib.SMTPException:
+        print("发送失败")
+    finally:
+        s.quit()
+
+
+def error_mail(user, content):
+    try:
+        msg_from = user["mail_from"]  # 发送方邮箱
+        passwd = user["mail_key"]  # 填入发送方邮箱的授权码
+        msg_to = user["mail_to"]  # 收件人邮箱
+        msg = get_mail(msg_from, msg_to, False, content)
+        s.login(msg_from, passwd)
+        s.sendmail(msg_from, msg_to, msg.as_string())
+        print("发送成功")
+    except smtplib.SMTPException:
+        print("发送失败")
+        email_logger.info('邮件发送失败')
+    finally:
+        s.quit()
