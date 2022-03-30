ba06707e7f00aa3d275ee166322b56562439b90c
nuclear06 <91713418+nuclear06@users.noreply.github.com>
2022-03-30 14:31:54 +0800

    Update checks.py

--- checks.py
+++ checks.py
@@ -1,4 +1,4 @@
 def message_check(message):
-    if message != '操作成功':
+    if not (message == '操作成功' or message ==r"call_user_func() expects parameter 1 to be a valid callback, function '/a_bupt/api/sso/cas' not found or invalid function name"):
         raise RuntimeWarning(message)
 
