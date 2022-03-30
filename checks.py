0e41f59878dd7328ebf1f150f64b156d3f8e2a18
nuclear06 <91713418+nuclear06@users.noreply.github.com>
2022-03-30 14:34:27 +0800

    Update checks.py

--- checks.py
+++ checks.py
@@ -2,3 +2,4 @@ def message_check(message):
     if not (message == '操作成功' or message ==r"call_user_func() expects parameter 1 to be a valid callback, function '/a_bupt/api/sso/cas' not found or invalid function name"):
         raise RuntimeWarning(message)
 
+#别问我为什么会有后面那个匹配，问就是信息黄泉服务器lj
